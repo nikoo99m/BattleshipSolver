1d9f4f1b35e922779acfd2cd2a450fbebe3c4742
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-09-14 18:19:08 +0100

    feat: Changing the representation of each cell.

--- src/Cell.java
+++ src/Cell.java
@@ -1,13 +1,13 @@
 public class Cell {
     private String status;
 
-    public static final String WATER = "~";//"\uD83C\uDF0A";//(char) 176 ;
-    public static final String HIT = "●";
-    public static final String MISS = "■";
+    public static final String WATER = "\uD83D\uDFE6";//""~";//"\uD83C\uDF0A";//(char) 176 ;
+    public static final String HIT = "\uD83D\uDCA5"; // "●";
+    public static final String MISS = "\u274C";
     private String shipShape;
-    public static final String SHIP = "⛴";//(char) 154;
+    public static final String SHIP = "⛵";//"⛴";//(char) 154;
 
-    public static final String SUNK = "\uD83D\uDFB3";
+    public static final String SUNK = "⬛";//"\uD83D\uDFB3";
     public String colourCode;
     private Ship ship;
 
